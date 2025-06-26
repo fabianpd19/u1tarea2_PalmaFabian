40cfa4a1b50a3e7310ee999933987ab8e7a1177f
fabianpd19 <fapalma1@espe.edu.ec>
2025-06-25 22:24:10 -0500

    Verificar que el middleware maneja correctamente excepciones inesperadas

--- src/middlewares/isLoggedIn.js
+++ src/middlewares/isLoggedIn.js
@@ -1,7 +1,12 @@
 module.exports = (req, res, next) => {
+  try {
+    // delete req.cookies;
     if (req.cookies.username) {
-        next();
-    } else {
-        res.redirect('/register');
+      return next();
     }
-}
+    res.redirect("/register");
+  } catch (error) {
+    console.error("Error en isLoggedIn middleware:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+};
